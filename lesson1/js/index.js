042b7ddf2aeddb889fba02722d3358b0bde60b5f
Hector Heriberto  Rodriguez Martinez <soybisonte@MacBook-Pro-de-Hector.local>
2020-02-21 18:32:04 -0600

    initial commit

---
+++ lesson1/js/index.js
@@ -0,0 +1,15 @@
+
+const radius = 30;
+
+function setup() {
+  createCanvas(640, 480);
+  background(0);
+}
+function draw() {
+
+  // let rndX = random(0, width)
+  // let rndY = random(0, height)
+  background(0);
+  noStroke();
+  ellipse(40, 120, radius, radius);
+}
