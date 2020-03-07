640165d6e0b6906c21bc4a6f25965d46357f8c71
Rodriguez Martinez Hector Heriberto <mb82922@MB82922MAC.local>
2020-03-07 12:16:32 -0600

    lesson 3 complete

--- lesson3/src/index.js
+++ lesson3/src/index.js
@@ -1,17 +1,45 @@
 import './style/app.styl'
 
-import * as P5 from 'p5'
+import p5 from 'p5'
 
-P5.setup() {
-  console.log('hello setup');
-  P5.createCanvas(640, 480);
-  P5.background(0);
+const s = (p5) => {
 
-}
-//
-P5.draw() {
-  console.log('hello draw');
-  P5.background(0);
-  P5.noStroke();
-  P5..ellipse(P5.mouseX, P5.mouseY, 20, 20)
-}
+  let x = 100;
+  let y = 100;
+  let slider;
+
+
+  p5.setup = () => {
+    p5.createCanvas(800, 800);
+  };
+
+  p5.draw = () => {
+    let middle = {
+      x: p5.width / 2,
+      y: p5.height / 2
+    }
+    p5.background(0, 30);
+    // p5.fill(255);
+    p5.noFill();
+    p5.stroke(255, 125, 0)
+    // p5.blendMode(p5.SOFT_LIGHT)
+    const n = 20;
+    let separation = 30;
+    let moveCoords = {
+      x: p5.mouseX - middle.x,
+      y: p5.mouseY - middle.y,
+    }
+    p5.rectMode(p5.CENTER)
+    const delta = 0.0012
+    p5.translate(middle.x, middle.y)
+      for (var i = 0; i < n; i++) {
+        p5.push()
+        // p5.rotate(p5.frameCount * delta * (i + 1) + 4 * delta * i)
+          p5.rect(0, 0, separation + (i * separation), separation + (i * separation));
+          p5.pop()
+      }
+  };
+
+};
+
+let myp5 = new p5(s);
